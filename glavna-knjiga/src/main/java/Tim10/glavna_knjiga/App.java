07399437b47ea608632b79308a6d3f09133fa081
faruk <faruk1337@gmail.com>
2015-05-16 21:19:05 +0200

    directory structure prep.

---
+++ glavna-knjiga/src/main/java/Tim10/glavna_knjiga/App.java
@@ -0,0 +1,13 @@
+package Tim10.glavna_knjiga;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
