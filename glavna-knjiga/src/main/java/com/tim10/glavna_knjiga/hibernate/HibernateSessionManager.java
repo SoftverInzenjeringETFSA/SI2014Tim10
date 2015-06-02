498aa4bed4e9dd4ed73e33bb0896f7d2478f6f59
faruk <faruk1337@gmail.com>
2015-06-02 02:10:02 +0200

    installation prep.

--- glavna-knjiga/src/main/java/com/tim10/glavna_knjiga/hibernate/HibernateSessionManager.java
+++ glavna-knjiga/src/main/java/com/tim10/glavna_knjiga/hibernate/HibernateSessionManager.java
@@ -1,31 +1,52 @@
 package com.tim10.glavna_knjiga.hibernate;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+import javax.imageio.spi.ServiceRegistry;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateSessionManager {
-	
-	private static final SessionFactory sessionFactory = buildSessionFactory();
-	 
-	@SuppressWarnings("deprecation")
-	private static SessionFactory buildSessionFactory() {
-		try {
-			// Create the SessionFactory from hibernate.cfg.xml
-			return new Configuration().configure().buildSessionFactory();
-		} catch (Exception ex) {
-			// Make sure you log the exception, as it might be swallowed
-			System.err.println("SessionFactory creation failed." + ex);
-			throw new ExceptionInInitializerError(ex);
-		}
-	}
- 
-	public static SessionFactory getSessionFactory() {
-		return sessionFactory;
-	}
- 
-	public static void shutdown() {
-		// Close caches and connection pools
-		getSessionFactory().close();
-	}
+
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    @SuppressWarnings("deprecation")
+    private static SessionFactory buildSessionFactory() {
+        /*
+        try {
+            java.util.Properties properties = new Properties();
+            properties.load(new FileInputStream("db.properties"));
+
+            Configuration configuration = new Configuration();
+
+            configuration.configure("hibernate.cfg.xml").addProperties(properties);;
+
+            return configuration.configure().buildSessionFactory();
+        } catch(Exception ex) {
+            System.err.println("SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError();
+        }
+        */
+        
+        try {
+            // Create the SessionFactory from hibernate.cfg.xml
+            return new Configuration().configure().buildSessionFactory();
+        } catch (Exception ex) {
+            // Make sure you log the exception, as it might be swallowed
+            System.err.println("SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+        
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        // Close caches and connection pools
+        getSessionFactory().close();
+    }
 
 }
